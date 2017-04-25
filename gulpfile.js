fd49ce7f1e96e2e39baff018df0700c06b18674c
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-04-25 21:38:21 +0100

    Edit readme.md file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var coverage = require('gulp-coverage');
+var serve = require('gulp-serve');
+var jasmineNode = require('gulp-jasmine-node');
 
 gulp.task('default', function() {
   // place code for your default task here
