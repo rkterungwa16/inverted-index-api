4ae55a95a8cbde973614e4e4d32f6f254eb85392
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-04-27 16:16:40 +0100

    Sample test pass for travis ci build

--- tests/inverted-index-testSpec.js
+++ tests/inverted-index-testSpec.js
@@ -1,50 +1,9 @@
 'use strict'
 var myApp = require('../src/inverted-index.js');
 
-describe('Inverted index class creates an index from a JSON array', function() {
-  describe('Read the JSON array of text objects', function() {
+describe('Sum()', function() {
     
-    it('should ensure the file content is actually a valid JSON array', function() {
-     
-    });
-
-    it('should ensure JSON array is not empty', function() {
-     
-    });
-
-    it('should return "Malformed" for malformed JSON array', function() {
-      
-    });
-
-    it('should return "Invalid" for invalid JSON array', function() {
-      
-    });
-
-    it('should return "Empty" for Empty JSON array', function() {
-      
-    });
+    it("counts one word", function() {
+    expect(myApp.sum(1, 2)).toEqual(3);
   });
-
-  describe('Populate index object with word tokens from JSON array', function() {
-
-    it('should create index for a given JSON file', function() {
-      
-    });
-
-    it('should return correct index for a given JSON file', function () {
-      
-    });
-  });
-
-  describe('Search index object with word tokens from JSON array', function() {
-
-    it('should create index for a given JSON file', function() {
-      
-    });
-
-    it('should return correct index for a given JSON file', function () {
-      
-    });
-  });
-
 });
