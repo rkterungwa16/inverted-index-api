9faec044791aadca7d5b563d32dab469763b48f1
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-04-27 01:45:31 +0100

    Fix gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,17 @@ const dotenv = require('dotenv').config({path: '.env.example'});
 const coverage = require('gulp-coverage');
 const nodemon = require('gulp-nodemon');
 const jasmineNode = require('gulp-jasmine-node');
+const jasminePhantomJS = require('gulp-jasmine-phantom');
+const istanbulReport = require('gulp-istanbul-report');
+
+const coverageFile = 'coverage/coverage.json';
+
+const jasminePhantomOpts = {
+  phantomjs: {
+    hooks: 'jasmine-phantomjs-istanbul',
+    coverageFile: coverageFile
+  }
+};
 
 // Task to run app server
 gulp.task('serve', () => {
@@ -14,17 +25,25 @@ gulp.task('serve', () => {
   })
 });
 
+gulp.task('test', function () {
+  gulp.src(['tests/inverted-index-testSpec.js'])
+    .pipe(jasminePhantomJS(jasminePhantomOpts))
+    .on('finish', function() {
+      gulp.src(coverageFile)
+        .pipe(istanbulReport())
+    });
+});
+
 gulp.task('run-tests', () => {
-    return gulp.src(['tests/**/*spec.js'])
+    return gulp.src(['tests/inverted-index-testSpec.js'])
     .pipe(jasmineNode());
 });
 
 // Task to generate coverage report
 gulp.task('coverage', () => {
-  return gulp.src('tests/**/*.js')
+  return gulp.src('tests/inverted-index-testSpec.js')
           .pipe(coverage.instrument({
-              pattern: ['src/**/*.js'],
-              debugDirectory: 'debug'
+              pattern: ['src/inverted-index.js']
           }))
           .pipe(jasmineNode())
           .pipe(coverage.gather())
---
+++ tests/inverted-index-testSpec.js
@@ -0,0 +1,50 @@
+'use strict'
+var myApp = require('../src/inverted-index.js');
+
+describe('Inverted index class creates an index from a JSON array', function() {
+  describe('Read the JSON array of text objects', function() {
+    
+    it('should ensure the file content is actually a valid JSON array', function() {
+     
+    });
+
+    it('should ensure JSON array is not empty', function() {
+     
+    });
+
+    it('should return "Malformed" for malformed JSON array', function() {
+      
+    });
+
+    it('should return "Invalid" for invalid JSON array', function() {
+      
+    });
+
+    it('should return "Empty" for Empty JSON array', function() {
+      
+    });
+  });
+
+  describe('Populate index object with word tokens from JSON array', function() {
+
+    it('should create index for a given JSON file', function() {
+      
+    });
+
+    it('should return correct index for a given JSON file', function () {
+      
+    });
+  });
+
+  describe('Search index object with word tokens from JSON array', function() {
+
+    it('should create index for a given JSON file', function() {
+      
+    });
+
+    it('should return correct index for a given JSON file', function () {
+      
+    });
+  });
+
+});
