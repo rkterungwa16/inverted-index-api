05f21e33d19f0045c3114c7b3fc9b57c163e3ee6
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-04-28 13:56:32 +0100

    (chore) Modify gulpfile.js and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,10 @@ const dotenv = require('dotenv').config({path: '.env.example'});
 const coverage = require('gulp-coverage');
 const nodemon = require('gulp-nodemon');
 const jasmineNode = require('gulp-jasmine-node');
-const jasminePhantomJS = require('gulp-jasmine-phantom');
 const istanbulReport = require('gulp-istanbul-report');
 const coveralls = require('gulp-coveralls');
 
 const coverageFile = 'coverage/coverage.json';
-
-const jasminePhantomOpts = {
-  phantomjs: {
-    hooks: 'jasmine-phantomjs-istanbul',
-    coverageFile: coverageFile
-  }
-};
 
 // Task to run app server
 gulp.task('serve', () => {
@@ -28,30 +20,26 @@ gulp.task('serve', () => {
 
 // Task to generate coverage report
 gulp.task('test', () => {
-  gulp.src(['tests/inverted-index-testSpec.js'])
-    .pipe(jasminePhantomJS(jasminePhantomOpts))
-    .on('finish', function() {
-      gulp.src(coverageFile)
-        .pipe(istanbulReport())
-    });
+  gulp.src(coverageFile)
+        .pipe(istanbulReport());
 });
 
 // Task to run tests 
 gulp.task('run-tests', () => {
-    return gulp.src(['tests/inverted-index-testSpec.js'])
+  return gulp.src(['tests/inverted-index-testSpec.js'])
     .pipe(jasmineNode());
 });
 
 // Task to generate coverage report
 gulp.task('coverage', () => {
   return gulp.src('tests/inverted-index-testSpec.js')
-          .pipe(coverage.instrument({
-              pattern: ['src/inverted-index.js']
-          }))
-          .pipe(jasmineNode())
-          .pipe(coverage.gather())
-          .pipe(coverage.format({ report: 'lcov' }))
-          .pipe(coveralls());
+    .pipe(coverage.instrument({
+        pattern: ['src/inverted-index.js']
+    }))
+    .pipe(jasmineNode())
+    .pipe(coverage.gather())
+    .pipe(coverage.format({ report: 'lcov' }))
+    .pipe(coveralls());
 });
 
 // Task to load code coverage to coveralls
