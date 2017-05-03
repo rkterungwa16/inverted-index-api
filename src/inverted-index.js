99b21f51484798fd34609d1714beadc11bce0089
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-05-03 09:54:47 +0100

    (chore) Modify invertedIndex class and testcases

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -7,20 +7,32 @@ class invertedIndex {
   /**
   * Initializes the Class with the required file name
   */
-  constructor(fileName) {
+  constructor(fileName, fileContent) {
     // assign the file name to object property
     this.fileName = fileName;
+    this.fileContent = fileContent;
   }
 
   /**
   * Reads a valid JSON data from given file
   * @returns {object} a JSON array
   */
-  getJson() {
+  __getJson() {
     // Return a valid json array
-    this.fileName;
-    const fileContent = this.fileName;
-    return fileContent;
+    return this.fileContent;
+  }
+  
+  /**
+  * Adds words not already in the array
+  * @return {boolean} a true if unique word
+  */
+  __addUniqueWords(data, arr) {
+    if (arr.indexOf(data) < 0) {
+      arr.push(data);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /**
@@ -28,10 +40,47 @@ class invertedIndex {
   * @return {object} an index object
   */
   createIndex() {
-    // Use json from getJson to create index
-    const myFile = this.fileName;
-    this.getJson();
-    let index;
+    let arrOfTitle = [];
+    let arrOfText = [];
+    let indexNumber= [];
+    let indexObj = {};
+    let index = {};
+    let allUniqueWords = [];
+    let jsonData;
+    jsonData = this.__getJson();
+
+
+
+    // Group title and text in all documents into separate arrays
+    for (let i=0; i<jsonData.length; i++) {
+      arrOfTitle.push(jsonData[i].title);
+      arrOfText.push(jsonData[i].text);
+      indexNumber.push(i);
+    }
+
+    // Create array collection of unique word tokens for each document
+    for (let k=0; k<jsonData.length; k++) {
+      let docContent = [];
+      docContent = arrOfTitle[k] + ' ' + arrOfText[k];
+      docContent = docContent.split(' ');
+      let uniqueWordsArr = [];
+        for (let q=0; q<docContent.length; q++) {
+            this.__addUniqueWords(docContent[q], uniqueWordsArr);
+        }
+        allUniqueWords.push(uniqueWordsArr);
+   }
+    
+    // Create index object
+    for (let s=0; s<allUniqueWords.length; s++) { 
+      for(let p=0; p<allUniqueWords[s].length; p++) {
+        if (allUniqueWords[s][p] in indexObj) {
+          indexObj[allUniqueWords[s][p]].push(s);
+        } else {
+            indexObj[allUniqueWords[s][p]] = [s];
+        }       
+      }
+    }
+    index[this.fileName] = indexObj;
     return index;
   }
   
--- tests/inverted-index-testSpec.js
+++ tests/inverted-index-testSpec.js
@@ -1,38 +1,36 @@
 import fs from 'fs';
 import myApp from '../src/inverted-index';
 
-const fileName = 'book.txt';
-const jsonOfFile = new myApp(fileName);
-
-const indexSample = {
-  an: [0],
-  inquiry: [0],
-  the: [0, 1],
-  string: [0, 1],
-  third: [1],
-  world: [1],
-  from: [1]
-};
-
-const fileContent = fs.readFileSync('fixtures/book.txt');
-const index = jsonOfFile.createIndex(fileName, fileContent);
+const fileName = 'book.json';
+const fileContent = JSON.parse(fs.readFileSync('fixtures/book.json'));
+const jsonOfFile = new myApp(fileName, fileContent);
 
 describe('Inverted index class', () => {
   describe('Read book data', () => {
     it("Should return 'invalid json' for fileContent = 'file content'", () => {
-      expect(jsonOfFile.getJson()).toEqual('invalid json');
+      expect(jsonOfFile.__getJson()).toEqual('invalid json');
     });
 
     it("Should return 'empty json' for fileContent = ''", () => {
-      expect(jsonOfFile.getJson()).toEqual('empty json');
+      expect(jsonOfFile.__getJson()).toEqual('empty json');
     });
 
     it("Should return 'malformed json' for fileContent = '[{ title: 'A' text: 'B' }]", () => {
-      expect(jsonOfFile.getJson()).toEqual('malformed json');
+      expect(jsonOfFile.__getJson()).toEqual('malformed json');
     });
 
-    it("Should return ''[{ 'title': 'A', 'text': 'B' }]'' for fileContent = '[{ title: 'A', text: 'B' }]'", () => {
-      expect(jsonOfFile.getJson()).toEqual([{ title: 'A', text: 'B' }]);
+    it("Should return a valid file content with the format [{ 'title': 'A', 'text': 'B' }]", () => {
+      expect(jsonOfFile.__getJson()).toEqual([
+        {
+          "title":"An inquiry into the wealth of nations",
+
+          "text":"This string seeks to help you understand the problem set"},
+        {
+          "title":"From third world to first world",
+
+          "text":"This string is also to help you understand the problem set"
+        }
+      ]);
     });
   });
 
@@ -56,33 +54,81 @@ describe('Inverted index class', () => {
 
   describe('Populate index', () => {
     it("Should return 'object' for typeof jsonOfFile.createIndex('fileName', 'fileContent')", () => {
-      expect(typeof jsonOfFile.createIndex(fileName, fileContent)).toEqual('object');
+      expect(typeof jsonOfFile.createIndex()).toEqual('object');
     });
 
-    it('Should return indexSample for createIndex(fileName, fileContent)', () => {
-      expect(jsonOfFile.createIndex(fileName, fileContent)).toEqual(indexSample);
-    });
+    it('Should return indexSample for createIndex()', () => {
+      expect(jsonOfFile.createIndex()).toEqual(
+        { 'book.json': 
+          {  An: [ 0 ],
+             inquiry: [ 0 ],
+             into: [ 0 ],
+             the: [ 0, 1 ],
+             wealth: [ 0 ],
+             of: [ 0 ],
+             nations: [ 0 ],
+             This: [ 0, 1 ],
+             string: [ 0, 1 ],
+             seeks: [ 0 ],
+             to: [ 0, 1 ],
+             help: [ 0, 1 ],
+             you: [ 0, 1 ],
+             understand: [ 0, 1 ],
+             problem: [ 0, 1 ],
+             set: [ 0, 1 ],
+             From: [ 1 ],
+             third: [ 1 ],
+             world: [ 1 ],
+             first: [ 1 ],
+             is: [ 1 ],
+             also: [ 1 ] 
+          } 
+        });
+          });
 
-    it("Should return '{'a': [1,2]}' for index[fileName]", () => {
-      expect(index[fileName]).toEqual({ a: [1, 2] });
+    it("Should return a valid output for createIndex().fileName", () => {
+      expect(createIndex.fileName).toEqual(
+        {  An: [ 0 ],
+           inquiry: [ 0 ],
+           into: [ 0 ],
+           the: [ 0, 1 ],
+           wealth: [ 0 ],
+           of: [ 0 ],
+           nations: [ 0 ],
+           This: [ 0, 1 ],
+           string: [ 0, 1 ],
+           seeks: [ 0 ],
+           to: [ 0, 1 ],
+           help: [ 0, 1 ],
+           you: [ 0, 1 ],
+           understand: [ 0, 1 ],
+           problem: [ 0, 1 ],
+           set: [ 0, 1 ],
+           From: [ 1 ],
+           third: [ 1 ],
+           world: [ 1 ],
+           first: [ 1 ],
+           is: [ 1 ],
+           also: [ 1 ] 
+        });
     });
   });
 
   describe('Search index', () => {
     it("Should return 'object' for typeof jsonOfFile.createIndex(fileName, fileContent)", () => {
-      expect(typeof jsonOfFile.createIndex(fileName, fileContent)).toEqual('object');
+      expect(typeof jsonOfFile.createIndex()).toEqual('object');
     });
 
     it("Should return { 'third world': [1] } for searchIndex(index, filename, 'Third World')", () => {
-      expect(jsonOfFile.searchIndex(fileName, fileContent)).toEqual({ 'third world': [1] });
+      expect(jsonOfFile.searchIndex()).toEqual({ 'third world': [1] });
     });
 
     it("Should return { 'third world': [1] } for searchIndex(index, fileName, ['Third', 'World'])", () => {
-      expect(jsonOfFile.searchIndex(index, fileName, ['Third', 'World'])).toEqual({ 'third world': [1] });
+      expect(jsonOfFile.searchIndex(['Third', 'World'])).toEqual({ 'third world': [1] });
     });
 
     it("Should return { 'third world': [1] } for searchIndex(index, fileName, 'Third', 'World')", () => {
-      expect(jsonOfFile.searchIndex(index, fileName, 'Third', 'World')).toEqual({ 'third world': [1] });
+      expect(jsonOfFile.searchIndex('Third', 'World')).toEqual({ 'third world': [1] });
     });
   });
 });
