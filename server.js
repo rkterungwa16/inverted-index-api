a2948fbc0fb443097e352535db142516e5ec3bc9
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-05-04 13:35:35 +0100

    Implement api for searchIndex and createIndex

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 import http from 'http';
 import dotenv from 'dotenv';
 import fs from 'fs';
-
+import url from 'url';
+import myApp from './src/inverted-index';
 
 dotenv.config({ path: '.env.example' });
+
+const file = 'fixtures/book.json';
+const jsonOfFile = new myApp('book.json');
+const term = 'third world';
+let Url;
+
+const apiUrls = (url) => {
+  if (url.pathname === '/api/createIndex') {
+    return jsonOfFile.createIndex()
+  } else if (url.pathname === '/api/searchIndex') {
+    return jsonOfFile.searchIndex(term);
+  } else {
+    return 'Please enter a valid url';
+  }
+};
+
+http.createServer((req, res) => {
+  //fs.createReadStream(file).pipe(res);
+  if (req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    Url = url.parse(req.url, true);
+    res.end(JSON.stringify(apiUrls(Url)));
+  } else {
+    res.writeHead(405);
+    res.end();
+  }
+}).listen(process.env.PORT_DEV, () => {
+  console.log('Server listening on', process.env.PORT_DEV);
+});
